811916566fff94d0bd4970f17d07b12887baf7da
jbouazao <jihad.bo2@gmail.com>
2020-07-05 20:06:43 +0100

    some memsize there

--- srcs/zjmp_op.c
+++ srcs/zjmp_op.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zjmp_op.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oelbelam <oelbelam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: myMac <myMac@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 12:24:53 by oelbelam          #+#    #+#             */
-/*   Updated: 2020/04/07 18:44:06 by oelbelam         ###   ########.fr       */
+/*   Updated: 2020/07/05 18:57:05 by myMac            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int zjmp_op(t_vm *vm, t_proc **prcs, t_proc **head, t_player **player)
 	(*prcs)->cur_pos = ((*prcs)->cur_pos + 1) % MEM_SIZE;
 	if ((*prcs)->carry == 1)
 	{
-		jump = vm->arena[((*prcs)->cur_pos) % 4096] << 8 | vm->arena[((*prcs)->cur_pos + 1) % 4096];
+		jump = vm->arena[((*prcs)->cur_pos) % MEM_SIZE] << 8 | vm->arena[((*prcs)->cur_pos + 1) % MEM_SIZE];
 		jump = jump % IDX_MOD;
 		(*prcs)->cur_pos = (((*prcs)->cur_pos - 1 + jump) + MEM_SIZE) % MEM_SIZE;
 	}
